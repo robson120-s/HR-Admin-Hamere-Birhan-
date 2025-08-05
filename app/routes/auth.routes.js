5aef48e63d8059f4e4e0329d7a074e9b84d03a56
robson120-s <shroba391@gmail.com>
2025-08-05 22:18:22 +0300

    new adding

---
+++ app/routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {login} =require('../controllers/auth.controller');
+const { PrismaClient } = require("../generated/prisma"); // ✅ Correct path to your generated client
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+
+
+router.post('/login', login);
+
+
+
+module.exports = router;
---
+++ app/routes/user.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { registerEmployee } = require('../controllers/user.controller');
+
+router.post('/register', registerEmployee);
+
+module.exports = router;
